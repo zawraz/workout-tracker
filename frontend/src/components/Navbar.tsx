d410a3b1760c7d6df285c0abd2c58ae916c6d8e5
Konrad Sawras <zawraz@gmail.com>
2023-11-30 14:32:12 +0100

    useLogout()
    * added useLogout() custom hook
    * fixed CSS nav styles

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
+import useLogout from "../hooks/useLogout"
 
 function Navbar() {
+	const { logout } = useLogout()
+	const handleClick = () => logout()
+
 	return (
 		<header>
 			<div className="container">
@@ -8,6 +12,9 @@ function Navbar() {
 					<h1>Workout tracker</h1>
 				</Link>
 				<nav>
+					<div>
+						<button onClick={handleClick}>Log out</button>
+					</div>
 					<div>
 						<Link to="/login">Login</Link>
 						<Link to="/register">Register</Link>
