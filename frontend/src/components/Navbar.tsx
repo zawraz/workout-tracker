c3cd39df75e7dd966b245db2fcd1c0c74c1b0711
Konrad Sawras <zawraz@gmail.com>
2023-12-01 12:18:33 +0100

    Display logged in user in the Navbar
    * + Navbar JSX logic to display proper buttons
    * fixed an AuthContext bug, resulting in user not being displayed

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom"
+import useAuthContext from "../hooks/useAuthContext"
 import useLogout from "../hooks/useLogout"
 
 function Navbar() {
 	const { logout } = useLogout()
+	const { user } = useAuthContext()
+
 	const handleClick = () => logout()
 
 	return (
@@ -12,13 +15,17 @@ function Navbar() {
 					<h1>Workout tracker</h1>
 				</Link>
 				<nav>
-					<div>
-						<button onClick={handleClick}>Log out</button>
-					</div>
-					<div>
-						<Link to="/login">Login</Link>
-						<Link to="/register">Register</Link>
-					</div>
+					{user ? (
+						<div>
+							<span>{user.email}</span>
+							<button onClick={handleClick}>Log out</button>
+						</div>
+					) : (
+						<div>
+							<Link to="/login">Login</Link>
+							<Link to="/register">Register</Link>
+						</div>
+					)}
 				</nav>
 			</div>
 		</header>
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react"
+import { createContext, useReducer, useEffect } from "react"
 
 export const AuthContext = createContext(null)
 
@@ -20,6 +20,14 @@ export function AuthContextProvider({
 	children: React.ReactNode
 }) {
 	const [state, dispatch] = useReducer(authReducer, { user: null })
+
+	useEffect(() => {
+		const user = JSON.parse(localStorage.getItem("user"))
+		if (user) {
+			dispatch({ type: "LOGIN", payload: user })
+		}
+	}, [])
+
 	console.log("AuthContext state: ", state)
 	return (
 		<AuthContext.Provider value={{ ...state, dispatch }}>
