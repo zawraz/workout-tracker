5768dfe156bedce822f488d0244dcd68af4c2fef
Konrad Sawras <zawraz@gmail.com>
2023-12-05 14:28:14 +0100

    useLogout() update
    * cleared the UI upon logging out to erase the previous data that would otherwise get displayed on slower networks

--- frontend/src/hooks/useLogout.ts
+++ frontend/src/hooks/useLogout.ts
@@ -1,11 +1,15 @@
 import useAuthContext from "./useAuthContext"
+import useWorkoutsContext from "./useWorkoutsContext"
 
 export default function useLogout() {
-	const { dispatch } = useAuthContext()
+	const { dispatch: authDispatch } = useAuthContext()
+	const { dispatch: workoutsDispatch } = useWorkoutsContext()
 
 	const logout = () => {
 		localStorage.removeItem("user")
-		dispatch({ type: "LOGOUT" })
+		authDispatch({ type: "LOGOUT" })
+
+		workoutsDispatch({ type: "SET_WORKOUTS", payload: null })
 	}
 
 	return { logout }
